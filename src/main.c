6763581f0f0d072b4b7abe51e9509ceadf6122a4
Philippe Thierry <phil@reseau-libre.net>
2018-10-10 13:05:48 +0200

    [enhancement] add datapath write plane

--- src/main.c
+++ src/main.c
@@ -19,8 +19,9 @@
  * without compiler complain. argc/argv is not a goot idea in term
  * of size and calculation in a microcontroler
  */
-uint8_t sdio_rd_buf[4000];
-uint8_t sdio_wr_buf[4000];
+#define SDIO_DEBUG 0
+#define SDIO_BUF_SIZE 8192
+uint8_t sdio_buf[SDIO_BUF_SIZE];
 
 int _main(uint32_t task_id)
 {
@@ -46,15 +47,15 @@ int _main(uint32_t task_id)
      *********************************************/
     dmashm_rd.target = id_crypto;
     dmashm_rd.source = task_id;
-    dmashm_rd.address = (physaddr_t)sdio_rd_buf;
-    dmashm_rd.size = 4000;
+    dmashm_rd.address = (physaddr_t)sdio_buf;
+    dmashm_rd.size = SDIO_BUF_SIZE;
     /* Crypto DMA will read from this buffer */
     dmashm_rd.mode = DMA_SHM_ACCESS_RD;
 
     dmashm_wr.target = id_crypto;
     dmashm_wr.source = task_id;
-    dmashm_wr.address = (physaddr_t)sdio_wr_buf;
-    dmashm_wr.size = 4000;
+    dmashm_wr.address = (physaddr_t)sdio_buf;
+    dmashm_wr.size = SDIO_BUF_SIZE;
     /* Crypto DMA will write into this buffer */
     dmashm_wr.mode = DMA_SHM_ACCESS_WR;
 
@@ -85,14 +86,12 @@ int _main(uint32_t task_id)
 
     do {
       ret = sys_ipc(IPC_SEND_SYNC, id_crypto, size, (char*)&ipc_sync_cmd);
-    } while (ret != SYS_E_DONE);
+    } while (ret == SYS_E_BUSY);
 
     /* Now wait for Acknowledge from Smart */
     id = id_crypto;
 
-    do {
-        ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_sync_cmd);
-    } while (ret != SYS_E_DONE);
+    ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_sync_cmd);
     if (   ipc_sync_cmd.magic == MAGIC_TASK_STATE_RESP
         && ipc_sync_cmd.state == SYNC_ACKNOWLEDGE) {
         printf("crypto has acknowledge end_of_init, continuing\n");
@@ -107,9 +106,7 @@ int _main(uint32_t task_id)
     id = id_crypto;
     size = 2;
 
-    do {
-        ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_sync_cmd);
-    } while (ret != SYS_E_DONE);
+    ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_sync_cmd);
 
     if (   ipc_sync_cmd.magic == MAGIC_TASK_STATE_CMD
         && ipc_sync_cmd.state == SYNC_READY) {
@@ -121,16 +118,31 @@ int _main(uint32_t task_id)
 
     ipc_sync_cmd.magic = MAGIC_TASK_STATE_RESP;
     ipc_sync_cmd.state = SYNC_READY;
-
     size = 2;
     do {
       ret = sys_ipc(IPC_SEND_SYNC, id_crypto, size, (char*)&ipc_sync_cmd);
-    } while (ret != SYS_E_DONE);
+    } while (ret == SYS_E_BUSY);
+    // take some time to finish all sync ipc...
+    sys_sleep(2000, SLEEP_MODE_INTERRUPTIBLE);
 
     /*******************************************
-     * Starting USB listener
+     * Sharing DMA SHM address and size with crypto
      *******************************************/
+    struct dmashm_info {
+        uint32_t addr;
+        uint16_t size;
+    };
 
+    struct dmashm_info dmashm_info;
+
+    dmashm_info.addr = (uint32_t)sdio_buf;
+    dmashm_info.size = SDIO_BUF_SIZE;
+
+    printf("informing crypto about DMA SHM...\n");
+    do {
+      ret = sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dmashm_info), (char*)&dmashm_info);
+    } while (ret == SYS_E_BUSY);
+    printf("Crypto informed.\n");
 
 
     /*******************************************
@@ -146,8 +158,41 @@ int _main(uint32_t task_id)
      * Main waiting loopt. The task main thread is awoken by any external
      * event such as ISR or IPC.
      */
+      struct dataplane_command dataplane_command_wr;
+      struct dataplane_command dataplane_command_ack = { DATA_WR_DMA_ACK, 0, 0 };
     while (1) {
-      sys_yield();
+      uint8_t id = id_crypto;
+      logsize_t size = sizeof(struct dataplane_command);
+
+      ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&dataplane_command_wr);
+      if (   ret == SYS_E_DONE
+          && id == id_crypto
+          && dataplane_command_wr.magic == DATA_WR_DMA_REQ) {
+
+#if SDIO_DEBUG
+          printf("received DMA write command to SDIO: @:%x size: %d\n",
+                 dataplane_command_wr.sector_address, dataplane_command_wr.num_sectors);
+#endif
+        // write request.... let's write then...
+        sd_write((uint32_t*)sdio_buf, dataplane_command_wr.sector_address, 512*dataplane_command_wr.num_sectors);
+
+            sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_ack);
+
+      }
+      else if (   ret == SYS_E_DONE
+               && id == id_crypto
+               && dataplane_command_wr.magic == DATA_RD_DMA_REQ) {
+#if SDIO_DEBUG
+          printf("received DMA read command to SDIO: @:%x size: %d\n",
+                 dataplane_command_wr.sector_address, dataplane_command_wr.num_sectors);
+#endif
+        // read request.... let's read then...
+
+          sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_ack);
+
+      }
+
+
     }
 
     return 0;
